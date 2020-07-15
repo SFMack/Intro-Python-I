1e8019d05aaf051eb686e44003e69b5bedab7bec
SFMack <sfcamack@icloud.com>
2020-07-14 22:24:06 -0700

    file_io complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,19 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+with open('./foo.txt', 'r+') as foo:
+    read_data = foo.read()
+    print(read_data)
+    foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('./bar.txt', 'w') as bar:
+    strings = ["this is string one",
+               "this is string two", "this is string three"]
+    for string in strings:
+        bar.write(string + '\n')
+    bar.close()
