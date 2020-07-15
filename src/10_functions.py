ef10af4dfe6977da027a6bac10477cdafae5f0e2
SFMack <sfcamack@icloud.com>
2020-07-14 20:50:28 -0700

    functions complete

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,24 @@
 # Write a function is_even that will return true if the passed-in number is even.
+def is_even():
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print('True')
+        return True
+    else:
+        print('False')
+        return False
 
-# YOUR CODE HERE
 
-# Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+is_even()
+
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
+def odd_or_even():
+    num = int(input("Enter another number: "))
+    if num % 2 == 0:
+        print("Even!")
+    else:
+        print("Odd")
 
-# YOUR CODE HERE
 
+odd_or_even()
