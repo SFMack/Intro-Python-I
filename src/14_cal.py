d8324d27d2119f5ed448129a3bde56ec1756d879
SFMack <sfcamack@icloud.com>
2020-07-15 17:52:57 -0700

    calendar updates

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,12 +18,12 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
@@ -32,7 +32,38 @@ import calendar
 from datetime import datetime
 
 
-def calender(**cal_args):
-    user_input = input(
-        "Enter a filename, month, and year seperated by commas: ")
-    print(user_input)
+def create_calender(**cal_args):
+    # add arguments to memory
+    items = cal_args.items()
+
+    # get current year from datetime module
+    current_date = datetime.now()
+
+    # loop through the given arguments tuple
+    for item in items:
+        items_length = len(items)
+        print(items_length)
+
+        if (items_length == 0):
+            # if a user gives no inputs, print calender for current month
+            print(calendar.month(current_date.year, current_date.month))
+            return
+        elif (items_length == 1):
+            # if user gives one input, assume its a month value
+            print(calendar.month(current_date.year, item[1]))
+            return
+        elif (items_length == 2):
+            # if a user gives two inputs, use both to create calendar
+            month, year = items
+            print(calendar.month(year[1], month[1]))
+            return
+        else:
+            print("Follow this format: create_calender(month, [year])")
+            return
+
+
+create_calender()  # should return current month and year
+create_calender(month=1)  # should return january
+create_calender(month=5, year=1991)  # should return may 1991
+# should return the `else` statement
+create_calender(month=5, year=1991, third=0)
